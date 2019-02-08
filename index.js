be0fd496f1be387bbb2ff8c559aed65672d9008a
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2019-02-08 21:17:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ client.on("ready", () => {
             if (message.channel.type === "dm") return;
             const prefixa = "";
             const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
-            command = args.shift().toLowerCase();
+            const command = args.shift().toLowerCase();
             const argsa = message.content.slice(prefixa.length).trim().split(/ +/g);
-            commanda = argsa.shift().toLowerCase();
+            const commanda = argsa.shift().toLowerCase();
 
             if (commanda === "رابط" || command === "رابط" || command === "link") {
                   message.channel.createInvite({
@@ -35,7 +35,6 @@ client.on("ready", () => {
             if (!size || currentSize !== size) channel.setName(`Voice ᎢᎡ [${currentSize}]`);
 
             const voice = oldMember.guild.channels.get("531906183758479360");
-            const voiceSupport = oldMember.guild.channels.get("531936102626361376");
             const role0 = oldMember.guild.roles.get("531415833465978890");
             if (newMember.voiceChannelID === voice.id) {
                   if (oldMember.guild.roles.has(role0.id)) {
@@ -218,201 +217,6 @@ client.on("ready", () => {
                                           url: oldMember.user.displayAvatarURL
                                     },
                                     title: "هناك مواطن غير مفعل في غرفة الانتظار",
-                                    fields: [
-                                          {
-                                                name: "User :",
-                                                value: `<@${oldMember.id}>`
-                                          }
-                                    ],
-                                    footer: {
-                                          text: oldMember.user.tag,
-                                          icon_url: oldMember.user.displayAvatarURL
-                                    },
-                                    timestamp: new Date()
-                              }
-                        }));
-                  }
-            } else if (newMember.voiceChannelID === voiceSupport.id) {
-                  if (oldMember.guild.roles.has(role0.id)) {
-                        newMember.guild.members.filter(m => m.roles.get("515528767272386561")).forEach(member => member.send({
-                              embed: {
-                                    color: 0x36393e,
-                                    author: {
-                                          name: member.user.username,
-                                          icon_url: member.user.displayAvatarURL
-                                    },
-                                    thumbnail: {
-                                          url: oldMember.user.displayAvatarURL
-                                    },
-                                    title: "هناك مواطن في غرفة الدعم الفني يحتاج الى المساعدة",
-                                    fields: [
-                                          {
-                                                name: "User :",
-                                                value: `<@${oldMember.id}>`
-                                          }
-                                    ],
-                                    footer: {
-                                          text: oldMember.user.tag,
-                                          icon_url: oldMember.user.displayAvatarURL
-                                    },
-                                    timestamp: new Date()
-                              }
-                        }));
-                        newMember.guild.members.filter(m => m.roles.get("531930530015608836")).forEach(member => member.send({
-                              embed: {
-                                    color: 0x36393e,
-                                    author: {
-                                          name: member.user.username,
-                                          icon_url: member.user.displayAvatarURL
-                                    },
-                                    thumbnail: {
-                                          url: oldMember.user.displayAvatarURL
-                                    },
-                                    title: "هناك مواطن في غرفة الدعم الفني يحتاج الى المساعدة",
-                                    fields: [
-                                          {
-                                                name: "User :",
-                                                value: `<@${oldMember.id}>`
-                                          }
-                                    ],
-                                    footer: {
-                                          text: oldMember.user.tag,
-                                          icon_url: oldMember.user.displayAvatarURL
-                                    },
-                                    timestamp: new Date()
-                              }
-                        }));
-                        newMember.guild.members.filter(m => m.roles.get("531930530925772811")).forEach(member => member.send({
-                              embed: {
-                                    color: 0x36393e,
-                                    author: {
-                                          name: member.user.username,
-                                          icon_url: member.user.displayAvatarURL
-                                    },
-                                    thumbnail: {
-                                          url: oldMember.user.displayAvatarURL
-                                    },
-                                    title: "هناك مواطن في غرفة الدعم الفني يحتاج الى المساعدة",
-                                    fields: [
-                                          {
-                                                name: "User :",
-                                                value: `<@${oldMember.id}>`
-                                          }
-                                    ],
-                                    footer: {
-                                          text: oldMember.user.tag,
-                                          icon_url: oldMember.user.displayAvatarURL
-                                    },
-                                    timestamp: new Date()
-                              }
-                        }));
-                        newMember.guild.members.filter(m => m.roles.get("531933725664084018")).forEach(member => member.send({
-                              embed: {
-                                    color: 0x36393e,
-                                    author: {
-                                          name: member.user.username,
-                                          icon_url: member.user.displayAvatarURL
-                                    },
-                                    thumbnail: {
-                                          url: oldMember.user.displayAvatarURL
-                                    },
-                                    title: "هناك مواطن في غرفة الدعم الفني يحتاج الى المساعدة",
-                                    fields: [
-                                          {
-                                                name: "User :",
-                                                value: `<@${oldMember.id}>`
-                                          }
-                                    ],
-                                    footer: {
-                                          text: oldMember.user.tag,
-                                          icon_url: oldMember.user.displayAvatarURL
-                                    },
-                                    timestamp: new Date()
-                              }
-                        }));
-                        newMember.guild.members.filter(m => m.roles.get("531930142478565376")).forEach(member => member.send({
-                              embed: {
-                                    color: 0x36393e,
-                                    author: {
-                                          name: member.user.username,
-                                          icon_url: member.user.displayAvatarURL
-                                    },
-                                    thumbnail: {
-                                          url: oldMember.user.displayAvatarURL
-                                    },
-                                    title: "هناك مواطن في غرفة الدعم الفني يحتاج الى المساعدة",
-                                    fields: [
-                                          {
-                                                name: "User :",
-                                                value: `<@${oldMember.id}>`
-                                          }
-                                    ],
-                                    footer: {
-                                          text: oldMember.user.tag,
-                                          icon_url: oldMember.user.displayAvatarURL
-                                    },
-                                    timestamp: new Date()
-                              }
-                        }));
-                        newMember.guild.members.filter(m => m.roles.get("532215364617109514")).forEach(member => member.send({
-                              embed: {
-                                    color: 0x36393e,
-                                    author: {
-                                          name: member.user.username,
-                                          icon_url: member.user.displayAvatarURL
-                                    },
-                                    thumbnail: {
-                                          url: oldMember.user.displayAvatarURL
-                                    },
-                                    title: "هناك مواطن في غرفة الدعم الفني يحتاج الى المساعدة",
-                                    fields: [
-                                          {
-                                                name: "User :",
-                                                value: `<@${oldMember.id}>`
-                                          }
-                                    ],
-                                    footer: {
-                                          text: oldMember.user.tag,
-                                          icon_url: oldMember.user.displayAvatarURL
-                                    },
-                                    timestamp: new Date()
-                              }
-                        }));
-                        newMember.guild.members.filter(m => m.roles.get("532215366366265355")).forEach(member => member.send({
-                              embed: {
-                                    color: 0x36393e,
-                                    author: {
-                                          name: member.user.username,
-                                          icon_url: member.user.displayAvatarURL
-                                    },
-                                    thumbnail: {
-                                          url: oldMember.user.displayAvatarURL
-                                    },
-                                    title: "هناك مواطن في غرفة الدعم الفني يحتاج الى المساعدة",
-                                    fields: [
-                                          {
-                                                name: "User :",
-                                                value: `<@${oldMember.id}>`
-                                          }
-                                    ],
-                                    footer: {
-                                          text: oldMember.user.tag,
-                                          icon_url: oldMember.user.displayAvatarURL
-                                    },
-                                    timestamp: new Date()
-                              }
-                        }));
-                        newMember.guild.members.filter(m => m.roles.get("532215369650274314")).forEach(member => member.send({
-                              embed: {
-                                    color: 0x36393e,
-                                    author: {
-                                          name: member.user.username,
-                                          icon_url: member.user.displayAvatarURL
-                                    },
-                                    thumbnail: {
-                                          url: oldMember.user.displayAvatarURL
-                                    },
-                                    title: "هناك مواطن في غرفة الدعم الفني يحتاج الى المساعدة",
                                     fields: [
                                           {
                                                 name: "User :",
