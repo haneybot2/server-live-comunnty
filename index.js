963335ba655dea1dd22c3196c3e4441a7ffb77c7
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2019-05-19 13:47:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ client.config = {
       prefix: "!",
       roles: ['531930530015608836', '531930530925772811', '531933725664084018', '531930142478565376', '532215364617109514', '532215366366265355', '532215369650274314', '515528767272386561']
 }
-
+const index = new Set();
+const index2 = new Set();
 client.on("ready", () => {
       console.log("ready man");
       console.log(`${client.user.tag} (${client.user.id})`);
@@ -15,31 +16,37 @@ client.on("ready", () => {
 			if (newMember.voiceChannel.id === '531906183758479360') {
 				if (!newMember.roles.has('483300860424749056') && newMember.roles.has('531415833465978890')) {
 					client.config.roles.forEach(role => {
-						newMember.guild.members.filter(m => m.roles.has(role)).forEach(mem => {
-							const embed = new RichEmbed()
-								.setColor(0x36393e)
-								.setAuthor(newMember.user.username, newMember.user.displayAvatarURL)
-								.setThumbnail(newMember.user.displayAvatarURL)
-								.setTitle("هناك مواطن غير مفعل في غرفة الانتظار")
-								.addField("User :", newMember.user)
-								.setFooter(mem.user.tag, mem.user.displayAvatarURL)
-								.setTimestamp()
-							mem.send(embed);
+						newMember.guild.members.filter(m => m.roles.has(role)).forEach(async mem => {
+							if (!index.has(mem)) {
+								const embed = new RichEmbed()
+									.setColor(0x36393e)
+									.setAuthor(newMember.user.username, newMember.user.displayAvatarURL)
+									.setThumbnail(newMember.user.displayAvatarURL)
+									.setTitle("هناك مواطن غير مفعل في غرفة الانتظار")
+									.addField("User :", newMember.user)
+									.setFooter(mem.user.tag, mem.user.displayAvatarURL)
+									.setTimestamp()
+								mem.send(embed);
+								await index.add(mem);
+							}
 						});
 					});
 				}
 			} else if (newMember.voiceChannel.id === '512917281940963328') {
 				client.config.roles.forEach(role => {
-					  newMember.guild.members.filter(m => m.roles.has(role)).forEach(mem => {
-						  const embed = new RichEmbed()
-								.setColor(0x36393e)
-								.setAuthor(newMember.user.username, newMember.user.displayAvatarURL)
-								.setThumbnail(newMember.user.displayAvatarURL)
-								.setTitle("هناك مواطن لديه مشكلة وينتظر احد ليحلها")
-								.addField("User :", newMember.user)
-								.setFooter(mem.user.tag, mem.user.displayAvatarURL)
-								.setTimestamp()
+					  newMember.guild.members.filter(m => m.roles.has(role)).forEach(async mem => {
+						  if (!index2.has(mem)) {
+							  const embed = new RichEmbed()
+									.setColor(0x36393e)
+									.setAuthor(newMember.user.username, newMember.user.displayAvatarURL)
+									.setThumbnail(newMember.user.displayAvatarURL)
+									.setTitle("هناك مواطن لديه مشكلة وينتظر احد ليحلها")
+									.addField("User :", newMember.user)
+									.setFooter(mem.user.tag, mem.user.displayAvatarURL)
+									.setTimestamp()
 							mem.send(embed);
+							  await index2.add(mem);
+						  }
 					  });
 				});
 			}
